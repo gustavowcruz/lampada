2d00aa5574dba433c80f812f239dad5d4a9ea975
Gustavo Wagner Cruz Cunha <164265839+gustavowcruz@users.noreply.github.com>
2024-12-03 22:44:35 -0300

    Add files via upload
    Um simples código utilizando JavaScript.
    
    Nele é possível ver dois botões, um em cima e outro em baixo, e uma figura de lâmpada. A figura da lâmpada pode mudar a medida que o usuário interage com os botôes, denotando assim a interatividade obtida através da implementação de um código em JavaScript.

---
+++ lightbulb/index.js
@@ -0,0 +1,9 @@
+// Função para ligar a luz
+function turnOn() {
+    document.getElementById('myImage').src = 'pic_bulbon.gif';
+}
+
+// Função para desligar a luz
+function turnOff() {
+    document.getElementById('myImage').src = 'pic_bulboff.gif';
+}
